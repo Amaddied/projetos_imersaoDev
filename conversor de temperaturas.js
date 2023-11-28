43ebec5ff31ddbcc96722ae93f6a907368eb301d
Amanda Serafim <98781484+Amaddied@users.noreply.github.com>
2023-11-28 13:06:37 -0300

    Create conversor de temperaturas.js

---
+++ conversor de temperaturas.js
@@ -0,0 +1,12 @@
+var tempEmCelcius = 30;
+var tempEmKelvin = tempEmCelcius + 273;
+var tempEmFahr = tempEmCelcius * 1.8 + 32;
+
+alert(
+  "Temperatura em Celcius: " +
+    tempEmCelcius +
+    "\n Temperatura convertida para Kelvin: " +
+    tempEmKelvin +
+    "\n Temperatura convertida para Fahrenheit: " +
+    tempEmFahr
+);
