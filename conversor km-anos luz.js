94d20ef8ce81babd8385558c1e8578cdcdbc4c35
Amanda Serafim <98781484+Amaddied@users.noreply.github.com>
2023-11-28 13:07:37 -0300

    Create conversor km-anos luz.js

---
+++ conversor km-anos luz.js
@@ -0,0 +1,10 @@
+var valorEmAnosLuz = 8.611;
+var medidaEmKm = 9461000000000; //quantidade de km em 1 ano luz
+
+var valorEmKm = valorEmAnosLuz * medidaEmKm;
+
+alert(
+  "A distância em km de SiriusA para a Terra é de " +
+    valorEmKm +
+    " quilômetros."
+);
